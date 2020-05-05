e2c179858b3769098b3b3cae4aed28121c788365
Michael Wettstein <chischte@gmx.ch>
2020-05-05 19:41:34 +0200

    BKP before erase orgy to find a bug

---
+++ src/CycleStep.h
@@ -0,0 +1,23 @@
+#ifndef CYCLESTEP_H
+#define CYCLESTEP_H
+#include "Arduino.h"
+
+class EzEPROM_Counter {
+public:
+  // FUNTIONS:
+  
+  // VARIABLES:
+  // n.a.
+
+private:
+  // FUNCTIONS:
+ 
+
+  // VARIABLES:
+ 
+
+};
+
+
+
+#endif
